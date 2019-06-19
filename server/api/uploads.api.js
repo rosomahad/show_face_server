3cd3df99bb0d41f3d13180e7883b6db1e0787530
Rosomakha <rosomakha.danil@gmail.com>
2019-06-19 22:41:33 +0300

    Add file upload route

--- server/api/uploads.api.js
+++ server/api/uploads.api.js
@@ -8,36 +8,59 @@ const path = require('path');
 const accessMiddleware = require('../middleware/access.middleware');
 
 const {
-    NotFoundError
+    WrongParametersError
 } = require('../lib/errors.js');
 
-const tmp_folder = path.resolve(__dirname, '../storage/tmp');
+const tmp_folder = path.resolve(__dirname, '../storage/tmp/');
 
 const router = express.Router();
 
+const AVATAR_IMG_CONFIGS = {
+    maxFileSize: 200 * 1024 * 1024,
+};
+
 router
-    .post('/avatars', accessMiddleware, async (req, res, next) => {
-        try {
-            const form = new formidable.IncomingForm();
+    .post('/avatars', async (req, res, next) => {
 
-            form.parse(req);
+        const form = new formidable.IncomingForm();
 
-            form.on('fileBegin', function (name, file) {
-                file.name = uuidv1();
+        Object.assign(AVATAR_IMG_CONFIGS, form);
 
-                file.path = tmp_folder + file.name;
+        form.on('fileBegin', function (name, file) {
 
-                res.json({
-                    status: 'success',
-                    data: {
-                        fileName: file.name,
-                    }
-                });
-            });
+            const extension = file.name.split('.').pop();
 
-        } catch (err) {
-            next(err);
-        }
+            const uid = uuidv1();
+
+            file.name = `${uid}.${extension}`;
+
+            file.path = path.resolve(tmp_folder, file.name);
+        });
+
+        form.parse(req, function (err, fields, files) {
+            if (err) next(err);
+
+            if (files['']) {
+                const file = files[''];
+                const mimeType = file.type;
+
+                if (mimeType === 'image/jpeg' || mimeType === 'image/png') {
+                    res.json({
+                        status: 'success',
+                        data: {
+                            fileName: tmp_folder + file.name
+                        }
+                    });
+                } else {
+                    fs.unlink(path.resolve(file.path), () => {
+                        res.json({
+                            status: 'error',
+                            message: 'Wrong file format'
+                        });
+                    });
+                }
+            }
+        });
     });
 
 module.exports = router;
